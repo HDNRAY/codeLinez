7d894564fb45fdea689171f28df9cd1534b1acd0
Ray <110681032@qq.com>
2021-12-13 14:26:22 +0800

    init project

---
+++ src/features/game/Game.tsx
@@ -0,0 +1,11 @@
+import './Game.scss';
+
+const Game = ()=>{
+    return <div className="game-wrapper">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+}
+
+export default Game;
